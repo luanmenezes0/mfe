102c14cab3a2c74c3de26e611025571c999a8077
Luan Menezes <luanmenezes99@gmail.com>
2021-09-24 11:34:04 -0300

    adss hello there

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <>
-      <h1>Hi</h1>
+      <h1>Hello there</h1>
       <hr />
       <MarketingApp />
     </>
