10d1d31fb763ba90824c960dabf99cfdbf737e43
Luan Menezes <luanmenezes99@gmail.com>
2021-09-27 17:31:53 -0300

    adds marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -17,7 +17,7 @@ const prodConfig = {
     new ModuleFederarionPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
